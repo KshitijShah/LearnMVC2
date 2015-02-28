33c2cdf08c41149146b2909b212fa2ecc9459942
KshitijShah <kgshah@gmail.com>
2015-02-28 08:01:35 -0800

    Leaning Routes
    Learning Routes

--- HelloWorld/HelloWorld/App_Start/RouteConfig.cs
+++ HelloWorld/HelloWorld/App_Start/RouteConfig.cs
@@ -13,11 +13,25 @@ namespace HelloWorld
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                name: "Home",
+                url: "",
+                defaults: new { controller = "Home", action = "GotoHome", id = UrlParameter.Optional }
+                );
+
+            routes.MapRoute(
+                name: "Home1",
+                url: "Home",
+                defaults: new { controller = "Home", action = "GotoHome", id = UrlParameter.Optional }
+                );
+
+            routes.MapRoute(
+                            name: "Default",
+                            url: "{controller}/{action}/{id}",
+                            defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+
+                        );
         }
     }
 }
