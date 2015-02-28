f1e77e8d4d6c97acaf82adf6e399360a3f06bf4b
KshitijShah <kgshah@gmail.com>
2015-02-28 09:31:47 -0800

    Created new page

--- HelloWorld/HelloWorld/Controllers/CustomerController.cs
+++ HelloWorld/HelloWorld/Controllers/CustomerController.cs
@@ -12,13 +12,34 @@ namespace HelloWorld.Controllers
         // GET: Customer
         public ActionResult Index()
         {
+            
 
-            CustomerData custData = new CustomerData();
-            custData.Id = 1001;
-            custData.CustomerCode = "2004";
-            custData.Amount = 1115.60;
+                CustomerData custData = new CustomerData();
+                custData.Id = 1001;
+                custData.CustomerCode = "2004";
+                custData.Amount = 1115.60;
 
-            return View("Customer",custData);
+                return View("Customer", custData);
+            
+        }
+
+        public ActionResult FillCustomer(CustomerData custData)
+        {
+
+            if (custData != null && custData.Id > 0)
+            {
+                return View("Customer", custData);
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+        public ActionResult SubmitCustomer()
+        {
+            return View();
         }
     }
 }
